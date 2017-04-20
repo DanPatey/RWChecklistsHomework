532886bd05c3c0a7f5639b817c927dd05307c817
Dan Patey <danpatey@danpatey.com>
2017-04-20 13:15:13 -0700

    Using IB to create delegates and extend textField input

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneBarButton.isEnabled = (newText.length > 0)
+        return true
+    }
+    
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func done(_ sender: Any) {
-        dismiss(animated: true, completion: nil)    
+        dismiss(animated: true, completion: nil)
     }
 }
